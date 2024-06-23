2ce02fbac0d39f24f0cca2c33c29b4dca6d62dc5
1F10230117 <s1f102301178@iniad.org>
2024-06-23 20:27:10 +0900

    テストを追加

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,5 +1,5 @@
 import unittest
-from project.calc import fact
+from project.calc import fact, gcd
 
 
 class TestTarget(unittest.TestCase):
@@ -13,3 +13,17 @@ class TestTarget(unittest.TestCase):
     def test_fact_negative(self):
         with self.assertRaises(ValueError):
             fact(-1)
+
+class TestTarget(unittest.TestCase):
+    def test_gcd_positive(self):
+        self.assertEqual(gcd(7, 3),1)
+        self.assertEqual(gcd(56, 98), 14)
+    
+    def test_gcd_negative(self):
+        self.assertEqual(gcd(-15, -25), -5)
+        self.assertEqual(gcd(-24, 32), 8)
+
+    def test_gcd_zero(self):
+        self.assertEqual(gcd(5, 0), 5)
+        self.assertEqual(gcd(0, 7), 7)
+        self.assertEqual(gcd(0, 0), 0)
