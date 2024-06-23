866e101ab1e6aefb0210e8b4ce81aedbf9a585df
1F10230117 <s1f102301178@iniad.org>
2024-06-23 21:04:46 +0900

    コードの整形

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -17,7 +17,7 @@ class TestTarget(unittest.TestCase):
     def test_gcd_positive(self):
         self.assertEqual(gcd(7, 3), 1)
         self.assertEqual(gcd(56, 98), 14)
-    
+
     def test_gcd_negative(self):
         self.assertEqual(gcd(-15, -25), 5)
         self.assertEqual(gcd(-24, 32), 8)
